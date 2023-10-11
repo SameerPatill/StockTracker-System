1a87ab02fd9fb5d26af0cf9755ba7acc2dd7b55a
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-10-11 11:43:17 +0530

    sidebar updates

--- client/src/Components/Sidebar/SideBar.jsx
+++ client/src/Components/Sidebar/SideBar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 const SideBar = () => {
+
+    const [sub_nav, setsub_nav] = useState(false)
+
+    function toggleSubNav() {
+        if (sub_nav) {
+            setsub_nav(false);
+        } else {
+            setsub_nav(true);
+        }
+    }
+
     return (
         <div className="sidebar">
             <div className="logo-details">
                 <i className='bx bxl-c-plus-plus'></i>
                 <span className="logo_name">Inventory</span>
             </div>
-            <ul className="nav-links">
+            <ul className="nav-links" >
                 <li>
                     <a href="#" className="active">
                         <i className='bx bx-grid-alt' ></i>
@@ -27,12 +38,41 @@ const SideBar = () => {
                         <span className="links_name">Inventory</span>
                     </a>
                 </li>
-                <li>
+                <li onClick={toggleSubNav}>
                     <a href="#">
                         <i className='bx bx-coin-stack' ></i>
                         <span className="links_name">Stock Control</span>
                     </a>
                 </li>
+                {sub_nav ? <div className='sub-nav'>
+                    <ul className="nav-links" style={{ 'margin-top': '0px' }}>
+                        <li><a href="#">
+                            <i class='bx bx-plus-circle'></i>
+                            <span className="links_name">Add Item</span>
+                        </a></li>
+
+                        <li>
+                            <a href="#">
+                                <i class='bx bxs-edit-alt'></i>
+                                <span className="links_name">Update Stock</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <i class='bx bx-minus-circle'></i>
+                                <span className="links_name">Delete an item</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class='bx bx-list-ul' ></i>
+                                <span className="links_name">View stock</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div> : ""}
+
                 <li>
                     <a href="#">
                         <i className='bx bx-book-alt' ></i>
